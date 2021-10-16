0ca9427e2e8c8703c81892b6856bb343f0e40485
Arifhusain Soudagar <38686334+Arizhusain@users.noreply.github.com>
2021-10-16 19:26:23 +0530

    Uploading Frontend design
    All the components for FE design and responsive design

---
+++ src/Pages/Login.js
@@ -0,0 +1,76 @@
+import styled from "styled-components";
+import { mobile } from "../responsive";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("https://i.pinimg.com/originals/40/80/68/408068a45d92c08ace8dd73670884744.jpg")
+      center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 25%;
+  padding: 20px;
+  background: white;
+  ${mobile({width:"70%"})}
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 10px 0px;
+  padding: 10px;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background: teal;
+  color: white;
+  cursor: pointer;
+  margin-bottom: 10px;
+`;
+const Link = styled.a`
+  margin: 5px 0px;
+  font-size: 12px;
+  text-decoration: underline;
+  cursor: pointer;
+`;
+
+const Login = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>SIGN IN</Title>
+        <Form>
+          <Input placeholder="Username" />
+          <Input placeholder="Password" />
+          <Button>LOGIN</Button>
+          <Link>Forgot password?</Link>
+          <Link>New User? CREATE AN ACCOUNT</Link>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Login;
--- src/Pages/ProductList.js
+++ src/Pages/ProductList.js
@@ -4,6 +4,7 @@ import Navbar from "../Components/Navbar";
 import Announcement from "../Components/Announcement";
 import Products from "../Components/Products";
 import Newsletter from "../Components/Newsletter";
+import { mobile } from "../responsive";
 
 const Container = styled.div``;
 
@@ -16,17 +17,20 @@ const FilterContainer = styled.div`
 
 const Filter = styled.div`
   margin: 20px;
+  ${mobile({width:"0px 20px", display:"flex", flexDirection:"column"})}
 `;
 
 const FilterText = styled.span`
   font-size: 20px;
   font-weight: 600;
   margin:right:20px;
+  ${mobile({ marginRight:"0px"})}
 `;
 
 const Select = styled.select`
     padding : 10px;
     margin-right:20px;
+    ${mobile({ margin:"10px 0px"})}
 `;
 const Options = styled.option``;
 
